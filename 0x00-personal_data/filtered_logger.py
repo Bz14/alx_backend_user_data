5da71e92ad4670fba676bb53e3e9ef64ecc47474
Bz14 <bezueyerusalem@gmail.com>
2024-11-09 19:08:14 +0300

    Files updated.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -24,7 +24,7 @@ class RedactingFormatter(logging.Formatter):
                             super().format(record), self.SEPARATOR)
 
 
-def filter_datum(fields: List[str], redaction: str,
+def filter_datum(fields, redaction: str,
                  message: str, separator: str) -> str:
     """
     Returns the log message obfuscated
