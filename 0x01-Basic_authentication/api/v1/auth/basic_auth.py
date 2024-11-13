2c3538b980fad05ecc38814da0c88bc512ecfde4
Bz14 <bezueyerusalem@gmail.com>
2024-11-13 11:34:30 +0300

    Files updated.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -39,14 +39,6 @@ class BasicAuth(Auth):
             return message
         except Exception:
             return None
-    """
-    Improve the method def extract_user_credentials
-    
-    (self, decoded_base64_authorization_header) to 
-    
-    allow password with :
-    
-    """
 
     def extract_user_credentials(self,
                                  decoded_base64_authorization_header:
@@ -59,7 +51,8 @@ class BasicAuth(Auth):
         if ":" not in decoded_base64_authorization_header:
             return None, None
         try:
-            user_credentials = decoded_base64_authorization_header.split(':',1)
+            user_credentials = decoded_base64_authorization_header.split(':',
+                                                                         1)
             return user_credentials[0], user_credentials[1]
         except Exception:
             return None, None
