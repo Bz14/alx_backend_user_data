c43af84c8454b6b7672c8f7118c7818ac829973c
Bz14 <bezueyerusalem@gmail.com>
2024-11-13 11:20:49 +0300

    Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 
 from .auth import Auth
 from typing import Tuple
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -54,3 +55,20 @@ class BasicAuth(Auth):
             return user_credentials[0], user_credentials[1]
         except Exception:
             return None, None
+
+    def user_object_from_credentials(self, user_email: str, user_pwd:
+                                     str) -> TypeVar('User'):
+        """User object from credentials"""
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
+            return None
+        if len(users) <= 0:
+            return None
+        if users[0].is_valid_password(user_pwd):
+            return users[0]
+        return None
