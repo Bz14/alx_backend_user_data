66c677b709d01e7f4e8959ee3b1227e891465c02
Bz14 <bezueyerusalem@gmail.com>
2024-11-16 11:23:15 +0300

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -53,10 +53,11 @@ def before_request():
                       '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if auth.require_auth(request.path, excluded_paths):
         request.current_user = auth.current_user(request)
-        if not auth.authorization_header(request):
-            abort(401)
-        if not auth.current_user(request):
-            abort(403)
+        if auth is None:
+            pass
+    elif not auth.require_auth(request.path, excluded_paths):
+        pass
+    else:
         if (auth.authorization_header(request) is None
                 and auth.session_cookie(request) is None):
             abort(401)
