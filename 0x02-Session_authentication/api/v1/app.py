c27fe6955aaa1ca0e678d85c93b09f6f7219bb74
Bz14 <bezueyerusalem@gmail.com>
2024-11-16 10:49:15 +0300

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -56,7 +56,7 @@ def before_request():
             abort(401)
         if not auth.current_user(request):
             abort(403)
-        if (auth.authorization_header(request)
+        if (not auth.authorization_header(request)
                 and not auth.session_cookie(request)):
             abort(401)
         request.current_user = auth.current_user(request)
