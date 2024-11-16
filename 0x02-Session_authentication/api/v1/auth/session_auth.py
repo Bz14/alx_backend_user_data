8aca9407d2a58ca73b3d5424371341d362da08b6
Bz14 <bezueyerusalem@gmail.com>
2024-11-16 10:33:56 +0300

    Session cookie

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -12,7 +12,7 @@ class SessionAuth(Auth):
         """ Create a session ID """
         if user_id is None or type(user_id) is not str:
             return None
-        session_id = uuid.uuid4()
+        session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
 
