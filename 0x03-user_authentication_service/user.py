3438b32d079c17c24f87464f0638290df6289bbc
Bz14 <bezueyerusalem@gmail.com>
2024-11-19 12:38:59 +0300

    0. User model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -11,8 +11,8 @@ Base = declarative_base()
 class User(Base):
     """ User class """
     __tablename__ = 'users'
-    id: Column = Column(Integer, primary_key=True)
-    email: Column = Column(String(256), nullable=False)
-    hashed_password: Column = Column(String(256), nullable=False)
-    session_id: Column = Column(String(256), nullable=True)
-    reset_token: Column = Column(String(256), nullable=True)
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
