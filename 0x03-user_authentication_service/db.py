b5dec23422c6c3fdea6faa1e788f990533a907b4
Bz14 <bezueyerusalem@gmail.com>
2024-11-19 14:02:31 +0300

    1. create user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -38,7 +38,7 @@ class DB:
             user = User(email=email, hashed_password=hashed_password)
             session.add(user)
             session.commit()
-        except (Exception):
+        except Exception:
             session.rollback()
             user = None
         return user
